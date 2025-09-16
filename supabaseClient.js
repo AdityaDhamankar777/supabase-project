724e55e1b7435a5402a4aa4db6255eb6067854e3
AdityaDhamankar777 <adityadhamankar063@gmail.com>
2025-09-16 17:20:40 +0530

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,7 +2,7 @@ import 'react-native-url-polyfill/auto'
 import { createClient } from '@supabase/supabase-js'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const SUPABASE_URL = 'https://YOUR_PROJECT.supabase.co'
+const SUPABASE_URL = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1bHdpbm5haHV1Z3pydGN3eXR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwMTkzNTQsImV4cCI6MjA3MzU5NTM1NH0.O74mjEkUiFdBrWaFU0dxFqrWHXoL5R-_yro-EPsBGX4'
 const SUPABASE_KEY = 'YOUR_ANON_KEY'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
@@ -13,3 +13,4 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
     detectSessionInUrl: false,
   },
 })
+
